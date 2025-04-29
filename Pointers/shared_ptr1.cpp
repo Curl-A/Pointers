eaf531765b2a866dcfd0bf75d86b20cb3c9056ff
Curl-A <aishukurle7975@gmai.com>
2025-04-28 23:39:03 -0400

    Commit of shared pointer implementation

--- Pointers/shared_ptr1.cpp
+++ Pointers/shared_ptr1.cpp
@@ -4,34 +4,32 @@ template<typename T>
 class shared_ptr {
     private:
         T* ptr;
+        int* ref_count=0;
 
     public:
-    static int ref_count; // static variable can be declared,we need to define & initialize outside of class
-
     //Constructor
-    shared_ptr(T n):ptr(new T(n)){
+    shared_ptr(T n):ptr(new T(n)),ref_count(new int(1)){
         std::cout<<"Constructor to create object with value "<<n<<" is called\n";
-        ref_count++;
+        ++(*ref_count);
     }
 
     //Copy Constructor
     shared_ptr(const shared_ptr& other){
         std::cout<<"Object copied\n";
         ptr=other.ptr;        //creating pointer with existing pointer value so that it can point to same mem loc
-        ref_count++;
+        ref_count = other.ref_count;
+        ++(*ref_count);
     }
 
     ~shared_ptr(){
         std::cout<<"Destuctor called\n";
-        if(ref_count==1){
+        if(--(*ref_count)==1){
             std::cout<<"Pointer "<<ptr<<" deleted\n";
             delete ptr;
+            delete ref_count;
         }
-        ref_count--;
     }
 };
-template<typename T>
-int shared_ptr<T>::ref_count=0;
 int main() {
     shared_ptr<int> sptr(10);
 
