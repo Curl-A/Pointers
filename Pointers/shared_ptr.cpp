b6018e5adb6f2859fce555b21166b26908ae7872
Curl-A <aishukurle7975@gmail.com>
2025-05-04 07:39:09 -0400

    Update shared_ptr.cpp

--- Pointers/shared_ptr.cpp
+++ Pointers/shared_ptr.cpp
@@ -17,7 +17,7 @@ class shared_ptr {
     //Copy Constructor
     shared_ptr(const shared_ptr& other){
         std::cout<<"Object copied\n";
-        ptr=other.ptr;        //creating pointer with existing pointer value so that it can point to same mem loc
+        ptr=other.ptr;        //creating pointer with existing pointer value so that it can point to same memory location
         ref_count++;
     }
 
@@ -34,10 +34,16 @@ template<typename T>
 int shared_ptr<T>::ref_count=0;
 int main() {
     shared_ptr<int> sptr(10);
-
     //shallow copy
     shared_ptr<int> sptr1(sptr);
-
     shared_ptr<int> sptr2(30);
+
+    // memory layout
+    
+    // objects sptr, sptr1, sptr2 will be stored on stack, these are just variables
+    // values 10 and 30 will be stored on heap memory
+    
+    // Here sptr(10) means it will create object "sptr" of shared_ptr class which will be stored on stack, 
+    // which in turn, creates a pointer (will be stored on stack) that points to the value 10 stored on heap memory.
 
 }
